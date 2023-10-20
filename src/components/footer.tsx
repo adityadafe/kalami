0315d47406663e3387ae11a984f90b79f5d0367d
KaKashi1210 <desmondaditya@gmail.com>
2023-10-20 18:09:45 +0530

    feat: landing page v1 complete

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+	return (
+		<footer className="mt-3 text-md text-center font-sans">
+			made with {`<3`} by {" "}
+			<a href="https://github.com/kakashi1210"
+				target="_blank"
+				className="font-extrabold "
+			>
+				kakashi
+			</a>
+		</footer>
+	)
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+	return (
+		<h1 className="font-black text-zinc-900 tracking-tight leading-none text-center 
+    text-[4rem] p-3	flex flex-row justify-center items-center
+		" >
+			kalami.
+		</h1>
+	)
+}
